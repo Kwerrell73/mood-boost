b2138f91906e862ec91a534ca0123b291efc065b
Kellie Werrell <kelliewerrell@gmail.com>
2021-04-17 16:06:30 -0700

    removed modal added buttons to main page and added event listeners

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,25 +1,14 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var natureBtn = document.getElementById('natureBtn');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+window.addEventListener('fetch', function (event) {
+    console.log("fetch add event listener");
+});
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.addEventListener('click', function (event) {
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&q=')
+    .then(data => {console.log(data)})
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+  
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
