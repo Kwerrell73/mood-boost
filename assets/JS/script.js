6132a0ea344355fcc926c8820f30fcfd584c371d
Kellie Werrell <kelliewerrell@gmail.com>
2021-04-17 16:34:01 -0700

    added event listener

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,13 +2,29 @@
 var natureBtn = document.getElementById('natureBtn');
 
 window.addEventListener('fetch', function (event) {
-    console.log("fetch add event listener");
+  console.log("fetch add event listener");
 });
 
-btn.addEventListener('click', function (event) {
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&q=')
-    .then(data => {console.log(data)})
-});
+natureBtn.addEventListener('click', function (event) {
+  fetch('https://api.giphy.com/v1/gifs/search?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&q=')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.data[0]);
+      // Create a variable that will select the <div> where the GIF will be displayed
+      var responseContainerEl = document.querySelector('#response-container');
 
-  
+      // Empty out the <div> before we append a GIF to it
+      responseContainerEl.innerHTML = "";
 
+      //
+      var gifImg = document.createElement('img');
+      gifImg.setAttribute('src', response.data[0]);
+      // Append 'gifImg' to the <div>
+      responseContainerEl.appendChild(gifImg);
+
+      //
+    });
+
+  });
