b56c06432f4a0e9f0cb9701285434e43eae7e794
VeeMcGee <vmcguire6377@outlook.com>
2021-04-19 12:10:08 -0600

    incorporating the most recent changes to develop branch into this branch

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,28 +1,99 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+  // Create event Listeners for each button -Kellie W
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  var natureBtn = document.getElementById('natureBtn');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  window.addEventListener('fetch', function (event) {
+    console.log("fetch add event listener");
+  });
+  
+  natureBtn.addEventListener('click', function (event) {
+    fetch(
+      'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&rating=g&tag=nature'
+    )
+      // Convert the response to JSON
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        // Use 'querySelector' to get the ID of where the GIF will be displayed
+        var responseContainerEl = document.querySelector('#response-container');
+        // Create an '<img>' element
+        var gifImg = document.createElement('img');
+        // Set that element's 'src' attribute to the 'image_url' from our Giphy API response
+        gifImg.setAttribute('src', response.data.image_url);
+        // Append the '<img>' element to the page
+        responseContainerEl.appendChild(gifImg);
+      });
+     
+  
+    });
+  
+    sportsBtn.addEventListener('click', function (event) {
+      fetch(
+        'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&rating=g&tag=sports'
+      )
+        // Convert the response to JSON
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          // Use 'querySelector' to get the ID of where the GIF will be displayed
+          var responseContainerEl = document.querySelector('#response-container');
+          // Create an '<img>' element
+          var gifImg = document.createElement('img');
+          // Set that element's 'src' attribute to the 'image_url' from our Giphy API response
+          gifImg.setAttribute('src', response.data.image_url);
+          // Append the '<img>' element to the page
+          responseContainerEl.appendChild(gifImg);
+        });
+       
+    
+      });
+  
+      animalBtn.addEventListener('click', function (event) {
+        fetch(
+          'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&rating=g&tag=baby animals'
+        )
+          // Convert the response to JSON
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (response) {
+            // Use 'querySelector' to get the ID of where the GIF will be displayed
+            var responseContainerEl = document.querySelector('#response-container');
+            // Create an '<img>' element
+            var gifImg = document.createElement('img');
+            // Set that element's 'src' attribute to the 'image_url' from our Giphy API response
+            gifImg.setAttribute('src', response.data.image_url);
+            // Append the '<img>' element to the page
+            responseContainerEl.appendChild(gifImg);
+          });
+         
+      
+        });
+  
+        surpriseBtn.addEventListener('click', function (event) {
+          fetch(
+            'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&rating=g&tag=laughing'
+          )
+            // Convert the response to JSON
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (response) {
+              // Use 'querySelector' to get the ID of where the GIF will be displayed
+              var responseContainerEl = document.querySelector('#response-container');
+              // Create an '<img>' element
+              var gifImg = document.createElement('img');
+              // Set that element's 'src' attribute to the 'image_url' from our Giphy API response
+              gifImg.setAttribute('src', response.data.image_url);
+              // Append the '<img>' element to the page
+              responseContainerEl.appendChild(gifImg);
+            });
+           
+        
+          });
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 //start of js for media player, vmcguire
 var media = document.querySelector('video');
